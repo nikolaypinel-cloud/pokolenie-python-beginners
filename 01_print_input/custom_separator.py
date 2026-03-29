fc985f1ad15e2e72a2c45339b542407437957fa6
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-03-29 18:47:59 +0300

    Create custom_separator.py

---
+++ 01_print_input/custom_separator.py
@@ -0,0 +1,12 @@
+"""
+Задача:
+Программа получает строку-разделитель и три строки.
+Выводит вторую, третью и четвёртую строки через разделитель.
+"""
+
+separator = input()
+line1 = input()
+line2 = input()
+line3 = input()
+
+print(line1, line2, line3, sep=separator)

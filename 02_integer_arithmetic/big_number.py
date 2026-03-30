e161e9962d7cb364127e183629e3d561a76a40c4
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-03-30 23:23:49 +0300

    Create big_number.py

---
+++ 02_integer_arithmetic/big_number.py
@@ -0,0 +1,9 @@
+"""
+Большое число
+Вычислить a^b + c^d, где a, b, c, d — целые положительные числа.
+"""
+
+# Решение
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+result = a**b + c**d
+print(result)

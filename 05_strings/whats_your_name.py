89a3e6ea9d7a180447486e2c54aca36eec24561f
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-21 03:22:45 +0300

    Create whats_your_name.py

---
+++ 05_strings/whats_your_name.py
@@ -0,0 +1,10 @@
+"""
+What's Your Name?
+Считать имя и фамилию, вывести приветствие.
+"""
+
+# Решение
+first_name = input()
+last_name = input()
+
+print("Hello", first_name, last_name + "! You have just delved into Python")

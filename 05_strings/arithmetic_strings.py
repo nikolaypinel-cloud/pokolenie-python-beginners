ea0ce04bc1996e85b66d6c67d2d3c143fc8d3448
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-22 01:34:23 +0300

    Update arithmetic_strings.py

--- 05_strings/arithmetic_strings.py
+++ 05_strings/arithmetic_strings.py
@@ -3,6 +3,10 @@
 Определить, можно ли из длин трёх строк построить арифметическую прогрессию.
 """
 
-# Решение
-a, b, c = sorted([len(input()) for _ in range(3)])
-print('YES' if b - a == c - b else 'NO')
+def solve(strings):
+    a, b, c = sorted([len(s) for s in strings])
+    return "YES" if b - a == c - b else "NO"
+
+if __name__ == "__main__":
+    strings = [input() for _ in range(3)]
+    print(solve(strings))

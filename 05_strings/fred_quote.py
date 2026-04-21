0018d7a821aa77798e437052456a749fbae0d4ce
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-21 03:20:33 +0300

    Create fred_quote.py

---
+++ 05_strings/fred_quote.py
@@ -0,0 +1,9 @@
+"""
+Цитата Фреда
+Вывести текст с использованием конкатенации строк.
+"""
+
+# Решение
+part1 = '"Python is a great language!", said Fred. '
+part2 = '"I don\'t ever remember having this much fun before."'
+print(part1 + part2)

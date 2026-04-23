451496be82e1638b45cb1298419ce8793aad939d
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-24 01:59:07 +0300

    Create sequence_print.py

---
+++ 07_for_loop/sequence_print.py
@@ -0,0 +1,14 @@
+"""
+Последовательность символов
+Вывести заданную последовательность, используя ровно три цикла for.
+"""
+
+# Решение
+for _ in range(6):
+    print("AAA")
+for _ in range(5):
+    print("BBBB")
+print("E")
+for _ in range(9):
+    print("TTTTT")
+print("G")

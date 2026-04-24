7071627074b4d5f2187e48a29ec33e9c0743537a
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-24 23:51:32 +0300

    Update numbers_ascending_descending.py

--- 07_for_loop/numbers_ascending_descending.py
+++ 07_for_loop/numbers_ascending_descending.py
@@ -5,7 +5,11 @@
 - в порядке убывания, если m >= n
 """
 
-# Решение
-m, n = (int(input()) for _ in range(2))
-nums = range(m, n + 1) if m < n else range(m, n - 1, -1)
-for num in nums: print(num)
+def solve(m, n):
+    nums = range(m, n + 1) if m < n else range(m, n - 1, -1)
+    return list(nums)
+
+if __name__ == "__main__":
+    m, n = (int(input()) for _ in range(2))
+    for num in solve(m, n):
+        print(num)

52af4810cf237edb7892bac92cc48222fb5139c9
nikolaypinel-cloud <nikolay.pinel@gmail.com>
2026-04-26 01:15:21 +0300

    Update factorial.py

--- 07_for_loop/factorial.py
+++ 07_for_loop/factorial.py
@@ -3,12 +3,12 @@
 Вычислить n! = 1 × 2 × 3 × ... × n
 """
 
-def solve(n):
-    factorial = 1
+def factorial(n):
+    result = 1
     for i in range(2, n + 1):
-        factorial *= i
-    return factorial
+        result *= i
+    return result
 
 if __name__ == "__main__":
     n = int(input())
-    print(solve(n))
+    print(factorial(n))
